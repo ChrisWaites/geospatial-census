a7207fcf89205961e4650e53f073db096b373fb8
Chris Waites <chriswaites@Chris-Waites-MacBook-Pro.local>
2018-10-02 12:48:06 -0400

    Update project structure.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from states import states
 from counties import counties
 from coords import coords
 
-file_name = 'data/usa_1930_5.csv'
+file_name = 'data/1930/usa_1930_5.csv'
 out_file_name = 'out/out.csv'
 
 os.makedirs('out', exist_ok=True)
@@ -14,6 +14,9 @@ with open(out_file_name, 'w') as g:
     with open(file_name) as f:
         reader = csv.DictReader(f)
         writer = csv.DictWriter(g, reader.fieldnames + ['latitude', 'longitude'])
+
+        print('Beginning Conversion...')
+
         for line in reader:
             try:
                 state_id = line['STATEICP']
